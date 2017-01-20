13d7dc5b0f8b78df8ece15a73946f7a8dac65b08
guptaa <arvindkgupta@outlook.com>
2017-01-20 16:25:43 +1000

    updated application

--- src/main/java/com/js/api/JsRestApiApplication.java
+++ src/main/java/com/js/api/JsRestApiApplication.java
@@ -2,6 +2,8 @@ package com.js.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
