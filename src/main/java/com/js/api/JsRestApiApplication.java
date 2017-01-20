8ef222fced7883610ff46c984dda4f50b3835ba2
guptaa <arvindkgupta@outlook.com>
2017-01-20 10:28:18 +1000

    extended SpringBootServletInitializer

--- src/main/java/com/js/api/JsRestApiApplication.java
+++ src/main/java/com/js/api/JsRestApiApplication.java
@@ -8,8 +8,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @Configuration
 @SpringBootApplication
 @EnableJpaRepositories
-public class JsRestApiApplication {
+public class JsRestApiApplication extends SpringBootServletInitializer {
 
+	 @Override
+    	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+       	 return application.sources(JsRestApiApplication.class);
+   	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JsRestApiApplication.class, args);
 	}
