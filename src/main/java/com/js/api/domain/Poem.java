49da3bf1f2d26bdace7d337455e05d9071d07b48
guptaa <arvindkgupta@outlook.com>
2017-01-20 16:24:39 +1000

    updated poem

--- src/main/java/com/js/api/domain/Poem.java
+++ src/main/java/com/js/api/domain/Poem.java
@@ -1,9 +1,13 @@
 package com.js.api.domain;
 
+import static javax.persistence.CascadeType.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.Data;
@@ -20,8 +24,9 @@ public class Poem {
 	@Column(name = "title")
 	private String title;
 
-	@Column(name = "album")
-	private Integer album;
+	@ManyToOne(optional = false, cascade = ALL)
+	@JoinColumn(name = "album")
+	private Album album;
 
 	@Column(name = "more_info")
 	private String moreInfo;
