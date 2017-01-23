7020f132e935c75b1af0af94a5343ed57a7de6dd
guptaa <arvindkgupta@outlook.com>
2017-01-23 10:55:12 +1000

    updated find by album method

--- src/main/java/com/js/api/repository/PoemRepository.java
+++ src/main/java/com/js/api/repository/PoemRepository.java
@@ -5,11 +5,12 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import com.js.api.domain.Album;
 import com.js.api.domain.Poem;
 
 @Repository
 public interface PoemRepository extends JpaRepository<Poem, Integer> {
 	
-	public List<Poem> findByAlbum(Integer id); 
+	public List<Poem> findByAlbum(Album album); 
 
 }
