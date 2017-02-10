66f42098150e155eb54965d757bf36f9488a7d39
guptaa <arvindkgupta@outlook.com>
2017-02-10 11:34:51 +1000

    removed @configuration

--- src/main/java/com/js/api/JsRestApiApplication.java
+++ src/main/java/com/js/api/JsRestApiApplication.java
@@ -5,10 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Configuration
 @SpringBootApplication
 @EnableJpaRepositories
 public class JsRestApiApplication extends SpringBootServletInitializer {
