fbc69f234fe8994134c22f74be67027d95223591
guptaa <arvindkgupta@outlook.com>
2017-02-10 11:31:54 +1000

    removed lombok

--- src/main/java/com/js/api/domain/Poem.java
+++ src/main/java/com/js/api/domain/Poem.java
@@ -10,10 +10,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
-@Data
 @Table(name = "poem")
 public class Poem {
 
@@ -33,4 +30,80 @@ public class Poem {
 
 	@Column(name = "other_albums")
 	private String otherAlbums;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the title
+	 */
+	public String getTitle() {
+		return this.title;
+	}
+
+	/**
+	 * @param title
+	 *            the title to set
+	 */
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	/**
+	 * @return the album
+	 */
+	public Album getAlbum() {
+		return this.album;
+	}
+
+	/**
+	 * @param album
+	 *            the album to set
+	 */
+	public void setAlbum(Album album) {
+		this.album = album;
+	}
+
+	/**
+	 * @return the moreInfo
+	 */
+	public String getMoreInfo() {
+		return this.moreInfo;
+	}
+
+	/**
+	 * @param moreInfo
+	 *            the moreInfo to set
+	 */
+	public void setMoreInfo(String moreInfo) {
+		this.moreInfo = moreInfo;
+	}
+
+	/**
+	 * @return the otherAlbums
+	 */
+	public String getOtherAlbums() {
+		return this.otherAlbums;
+	}
+
+	/**
+	 * @param otherAlbums
+	 *            the otherAlbums to set
+	 */
+	public void setOtherAlbums(String otherAlbums) {
+		this.otherAlbums = otherAlbums;
+	}
+
 }
