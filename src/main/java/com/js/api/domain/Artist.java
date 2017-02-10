84c9779617678d81b0894d3de35505000816aeb3
guptaa <arvindkgupta@outlook.com>
2017-02-10 11:30:35 +1000

    Removed lombok

--- src/main/java/com/js/api/domain/Artist.java
+++ src/main/java/com/js/api/domain/Artist.java
@@ -8,10 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
-@Data
 @Table(name = "Artist")
 public class Artist {
 
@@ -45,4 +42,155 @@ public class Artist {
 
 	@Column(name = "other_names")
 	private String otherNames;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return this.name;
+	}
+
+	/**
+	 * @param name
+	 *            the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the bornOn
+	 */
+	public LocalDate getBornOn() {
+		return this.bornOn;
+	}
+
+	/**
+	 * @param bornOn
+	 *            the bornOn to set
+	 */
+	public void setBornOn(LocalDate bornOn) {
+		this.bornOn = bornOn;
+	}
+
+	/**
+	 * @return the diedOn
+	 */
+	public LocalDate getDiedOn() {
+		return this.diedOn;
+	}
+
+	/**
+	 * @param diedOn
+	 *            the diedOn to set
+	 */
+	public void setDiedOn(LocalDate diedOn) {
+		this.diedOn = diedOn;
+	}
+
+	/**
+	 * @return the moreInfo
+	 */
+	public String getMoreInfo() {
+		return this.moreInfo;
+	}
+
+	/**
+	 * @param moreInfo
+	 *            the moreInfo to set
+	 */
+	public void setMoreInfo(String moreInfo) {
+		this.moreInfo = moreInfo;
+	}
+
+	/**
+	 * @return the introduction
+	 */
+	public String getIntroduction() {
+		return this.introduction;
+	}
+
+	/**
+	 * @param introduction
+	 *            the introduction to set
+	 */
+	public void setIntroduction(String introduction) {
+		this.introduction = introduction;
+	}
+
+	/**
+	 * @return the activityYears
+	 */
+	public String getActivityYears() {
+		return this.activityYears;
+	}
+
+	/**
+	 * @param activityYears
+	 *            the activityYears to set
+	 */
+	public void setActivityYears(String activityYears) {
+		this.activityYears = activityYears;
+	}
+
+	/**
+	 * @return the occupation
+	 */
+	public String getOccupation() {
+		return this.occupation;
+	}
+
+	/**
+	 * @param occupation
+	 *            the occupation to set
+	 */
+	public void setOccupation(String occupation) {
+		this.occupation = occupation;
+	}
+
+	/**
+	 * @return the pics
+	 */
+	public String getPics() {
+		return this.pics;
+	}
+
+	/**
+	 * @param pics
+	 *            the pics to set
+	 */
+	public void setPics(String pics) {
+		this.pics = pics;
+	}
+
+	/**
+	 * @return the otherNames
+	 */
+	public String getOtherNames() {
+		return this.otherNames;
+	}
+
+	/**
+	 * @param otherNames
+	 *            the otherNames to set
+	 */
+	public void setOtherNames(String otherNames) {
+		this.otherNames = otherNames;
+	}
+
 }
