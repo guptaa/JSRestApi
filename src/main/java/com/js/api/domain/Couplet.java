f71234b5504aae61aaf7dc5f6197c879b70f2428
guptaa <arvindkgupta@outlook.com>
2017-02-10 11:31:01 +1000

    removed lombok

--- src/main/java/com/js/api/domain/Couplet.java
+++ src/main/java/com/js/api/domain/Couplet.java
@@ -6,10 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.Data;
-
 @Entity
-@Data
 @Table(name = "couplet")
 public class Couplet {
 
@@ -31,5 +28,95 @@ public class Couplet {
 
 	@Column(name = "poem")
 	private Integer poem;
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return this.id;
+	}
+
+	/**
+	 * @param id
+	 *            the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return this.description;
+	}
+
+	/**
+	 * @param description
+	 *            the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the poet
+	 */
+	public Integer getPoet() {
+		return this.poet;
+	}
+
+	/**
+	 * @param poet
+	 *            the poet to set
+	 */
+	public void setPoet(Integer poet) {
+		this.poet = poet;
+	}
+
+	/**
+	 * @return the otherSinger
+	 */
+	public Integer getOtherSinger() {
+		return this.otherSinger;
+	}
+
+	/**
+	 * @param otherSinger
+	 *            the otherSinger to set
+	 */
+	public void setOtherSinger(Integer otherSinger) {
+		this.otherSinger = otherSinger;
+	}
+
+	/**
+	 * @return the composer
+	 */
+	public Integer getComposer() {
+		return this.composer;
+	}
+
+	/**
+	 * @param composer
+	 *            the composer to set
+	 */
+	public void setComposer(Integer composer) {
+		this.composer = composer;
+	}
+
+	/**
+	 * @return the poem
+	 */
+	public Integer getPoem() {
+		return this.poem;
+	}
+
+	/**
+	 * @param poem
+	 *            the poem to set
+	 */
+	public void setPoem(Integer poem) {
+		this.poem = poem;
+	}
 
 }
